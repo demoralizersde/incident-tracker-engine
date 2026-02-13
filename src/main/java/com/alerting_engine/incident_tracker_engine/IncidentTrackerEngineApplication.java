0e3ad85ba48dffcaac5ffaa1a475e9b2e787cb1d
unknown <amanksingh11111@gmail.com>
2026-02-13 22:51:14 +0530

    Initial Commit

---
+++ src/main/java/com/alerting_engine/incident_tracker_engine/IncidentTrackerEngineApplication.java
@@ -0,0 +1,13 @@
+package com.alerting_engine.incident_tracker_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentTrackerEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentTrackerEngineApplication.class, args);
+	}
+
+}
